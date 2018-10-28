e84b9b3b0730d162e601609b7ac1e68d7b86dea4
spark <sparkdev@163.com>
2018-10-28 12:37:43 +0800

    add unit case.

--- app_test.go
+++ app_test.go
@@ -19,3 +19,11 @@ func Test_GetNameLen_2(t *testing.T) {
 		t.Log("test passed.")
 	}
 }
+
+func Test_GetNameLen_3(t *testing.T) {
+	if l := getNameLen("andrew"); l != 5 {
+		t.Error("test failed, the length of andrew string is not correct.")
+	} else {
+		t.Log("test passed.")
+	}
+}
