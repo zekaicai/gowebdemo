374121832788728ac9a676df273c6887950b4589
spark <sparkdev@163.com>
2019-10-15 12:20:58 +0800

    add log to console

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func getNameLen(name string) int {
@@ -13,7 +14,8 @@ func getNameLen(name string) int {
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message + " : " + fmt.Sprintf("%d", getNameLen(message))
+	message = "Hello " + message + " : " + fmt.Sprintf("%d", getNameLen(message)) + "\n"
+	fmt.Print(getDayString() + " " + message)
 	w.Write([]byte(message))
 }
 
@@ -22,4 +24,11 @@ func main() {
 	if err := http.ListenAndServe(":8088", nil); err != nil {
 		panic(err)
 	}
+}
+
+func getDayString() string {
+	nowTime := time.Now()
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	chTime := nowTime.In(loc)
+	return chTime.Format("20060102150405")
 }
